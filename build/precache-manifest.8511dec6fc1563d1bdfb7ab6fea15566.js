c18c7b0e65488bd7c06abba325591af218787ec2
Victor <victorribera92@gmail.com>
2020-05-29 17:07:49 +0200

    deploy 2

---
+++ build/precache-manifest.8511dec6fc1563d1bdfb7ab6fea15566.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4962156953325aa4711ffab24b9bbeb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9669034012f07019882",
+    "url": "/static/js/2.2f66f608.chunk.js"
+  },
+  {
+    "revision": "fab53202193c96bf1dd7",
+    "url": "/static/js/main.3fd2aa26.chunk.js"
+  },
+  {
+    "revision": "0e20a33db24634f3aa93",
+    "url": "/static/js/runtime-main.cf165af6.js"
+  }
+]);
